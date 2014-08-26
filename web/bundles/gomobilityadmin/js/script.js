f4fdf055c27c9a7fa907751df1420da3b44e6b3d
laurent gourmelon <laurentgourmelon@luckycart.com>
2014-08-26 14:42:06 +0200

    dashboard in progress

--- web/bundles/gomobilityadmin/js/script.js
+++ web/bundles/gomobilityadmin/js/script.js
@@ -1,8 +1,35 @@
-(function() {
-	var windowHeight 	= $(window).height();
-	var windowWidth		= $(window).width();
-	var navWidth		= $('nav').width();
-	var headerHeight	= $('header').height();
-	var content 		= $('.content');
+$(document).ready(function(){
+	
+	window.onload = init;
 
-})();
+	function init (){
+		reziseElmt();
+		diaNb();
+	}
+
+	function reziseElmt (argument) {
+		var windowHeight 	= $(window).height();
+		var windowWidth		= $(window).width();
+		var navWidth		= $('nav').width();
+		var headerHeight	= $('header').height();
+		var content 		= $('.content');
+
+		content.css({
+			'padding-left':navWidth,
+			'padding-top':headerHeight
+		});
+	}
+
+	function diaNb () {
+		var sportNb = 10;
+		var tourismeNb = 70;
+		var gotoworkNb = 20;
+
+		$('.sport .dia-ct').height(sportNb+'%');
+		$('.tourisme .dia-ct').height(tourismeNb+'%');
+		$('.gotowork .dia-ct').height(gotoworkNb+'%');
+	}
+
+});
+
+
